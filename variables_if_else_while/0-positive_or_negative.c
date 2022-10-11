174f23632704ad7af109b5ed5e92e3a759284ffe
GitHolbies <GitHolbies@github.com>
2022-10-11 10:19:54 -0700

    positiveornegative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,14 +2,22 @@
 
 #include <time.h>
 
-   /* more headers goes there */
+#include <stdio.h>
 
 
 
-   /* betty style doc for function main goes there */
+/**
+
+ * main - Determines if a number is positive, negative or zero.
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
 
 int main(void)
-
+  
 {
   
   int n;
@@ -20,8 +28,30 @@ int main(void)
   
   n = rand() - RAND_MAX / 2;
   
-  /* your code goes there */
- 
-  return (n);
+  if (n > 0)
+    
+    {
+      
+      printf("%d is positive\n", n);
+      
+    }
+  
+  else if (n == 0)
+    
+    {
+      
+      printf("%d is neutral\n", n);
+      
+    }
+  
+  else
+    
+    {
+      
+      printf("%d is negative\n", n);
+      
+    }
+  
+  return (0);
   
 }
